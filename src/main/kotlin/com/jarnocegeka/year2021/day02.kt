d693b53497a176444899898ccd2b12fabddccee6
Michiels <jarno.michiels@cegeka.com>
2021-12-02 06:12:50 +0100

    Advent of Code 2021: Day 02

--- src/main/kotlin/com/jarnocegeka/year2021/day02.kt
+++ src/main/kotlin/com/jarnocegeka/year2021/day02.kt
@@ -4,8 +4,40 @@ import com.jarnocegeka.utils.readInputFileLines
 
 fun adventOfCodeYear2021Day02Part1() {
     val lines = readInputFileLines("InputYear2021Day02.txt")
+
+    var position = 0
+    var depth = 0
+    lines.map { Pair(it.substringBefore(' '), it.substringAfter(' ').toInt()) }.forEach {
+        when (it.first) {
+            "forward" -> position += it.second
+            "up" -> depth -= it.second
+            "down" -> depth += it.second
+        }
+    }
+
+    println("Position: $position")
+    println("Depth: $depth")
+    println("Result: ${position * depth}")
 }
 
 fun adventOfCodeYear2021Day02Part2() {
     val lines = readInputFileLines("InputYear2021Day02.txt")
+
+    var position = 0
+    var depth = 0
+    var aim = 0
+    lines.map { Pair(it.substringBefore(' '), it.substringAfter(' ').toInt()) }.forEach {
+        when (it.first) {
+            "forward" -> {
+                position += it.second
+                depth += (aim * it.second)
+            }
+            "up" -> aim -= it.second
+            "down" -> aim += it.second
+        }
+    }
+
+    println("Position: $position")
+    println("Depth: $depth")
+    println("Result: ${position * depth}")
 }
--- src/main/kotlin/com/jarnocegeka/year2021/main2021.kt
+++ src/main/kotlin/com/jarnocegeka/year2021/main2021.kt
@@ -3,6 +3,6 @@ package com.jarnocegeka.year2021
 fun main() {
 //    adventOfCodeYear2021Day01Part1()
 //    adventOfCodeYear2021Day01Part2()
-    adventOfCodeYear2021Day02Part1()
-//    adventOfCodeYear2021Day02Part2()
+//    adventOfCodeYear2021Day02Part1()
+    adventOfCodeYear2021Day02Part2()
 }
