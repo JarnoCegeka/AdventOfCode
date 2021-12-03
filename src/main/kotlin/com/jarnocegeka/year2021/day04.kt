0f4883ba61910c8c2c301416ce0fb32d7a7b7c36
Michiels <jarno.michiels@cegeka.com>
2021-12-03 06:57:42 +0100

    Advent of Code 2021: Day 04 setup

---
+++ src/main/kotlin/com/jarnocegeka/year2021/day04.kt
@@ -0,0 +1,11 @@
+package com.jarnocegeka.year2021
+
+import com.jarnocegeka.utils.readInputFileLines
+
+fun adventOfCodeYear2021Day04Part1() {
+    val lines = readInputFileLines("InputYear2021Day04.txt")
+}
+
+fun adventOfCodeYear2021Day04Part2() {
+    val lines = readInputFileLines("InputYear2021Day04.txt")
+}
--- src/main/kotlin/com/jarnocegeka/year2021/main2021.kt
+++ src/main/kotlin/com/jarnocegeka/year2021/main2021.kt
@@ -6,5 +6,7 @@ fun main() {
 //    adventOfCodeYear2021Day02Part1()
 //    adventOfCodeYear2021Day02Part2()
 //    adventOfCodeYear2021Day03Part1()
-    adventOfCodeYear2021Day03Part2()
+//    adventOfCodeYear2021Day03Part2()
+    adventOfCodeYear2021Day04Part1()
+//    adventOfCodeYear2021Day04Part2()
 }
