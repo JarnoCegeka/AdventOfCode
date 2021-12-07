9673853c5052064ec3eeef1a512fc5f0d9b67228
Michiels <jarno.michiels@cegeka.com>
2021-12-07 06:45:31 +0100

    Advent of Code 2021: Day 07

--- src/main/kotlin/com/jarnocegeka/year2021/day07.kt
+++ src/main/kotlin/com/jarnocegeka/year2021/day07.kt
@@ -1,11 +1,46 @@
 package com.jarnocegeka.year2021
 
 import com.jarnocegeka.utils.readInputFileLines
+import java.util.Comparator.comparing
 
 fun adventOfCodeYear2021Day07Part1() {
-    val lines = readInputFileLines("InputYear2021Day07.txt")
+    val positions = readInputFileLines("InputYear2021Day07.txt")[0].split(',').map { it.toInt() }
+    val minPosition = positions.minOrNull()!!
+    val maxPosition = positions.maxOrNull()!!
+
+    val positionCounters = mutableMapOf<Int, Int>()
+    (minPosition..maxPosition).forEach { possiblePosition ->
+        if (!positionCounters.containsKey(possiblePosition)) {
+            var counter = 0
+            positions.forEach {
+                counter += if (possiblePosition < it) it - possiblePosition else possiblePosition - it
+            }
+            positionCounters[possiblePosition] = counter
+        }
+    }
+
+    val bestPosition = positionCounters.minOfWith(comparing { it.value }) { it }
+    println("Result: $bestPosition")
 }
 
 fun adventOfCodeYear2021Day07Part2() {
-    val lines = readInputFileLines("InputYear2021Day07.txt")
+    val positions = readInputFileLines("InputYear2021Day07.txt")[0].split(',').map { it.toInt() }
+    val minPosition = positions.minOrNull()!!
+    val maxPosition = positions.maxOrNull()!!
+
+    val positionCounters = mutableMapOf<Int, Int>()
+    (minPosition..maxPosition).forEach { possiblePosition ->
+        if (!positionCounters.containsKey(possiblePosition)) {
+            var counter = 0
+            positions.forEach {
+                val difference = if (possiblePosition < it) it - possiblePosition else possiblePosition - it
+                val differenceSum = (0..difference).sum()
+                counter += differenceSum
+            }
+            positionCounters[possiblePosition] = counter
+        }
+    }
+
+    val bestPosition = positionCounters.minOfWith(comparing { it.value }) { it }
+    println("Result: $bestPosition")
 }
--- src/main/kotlin/com/jarnocegeka/year2021/main2021.kt
+++ src/main/kotlin/com/jarnocegeka/year2021/main2021.kt
@@ -13,6 +13,6 @@ fun main() {
 //    adventOfCodeYear2021Day05Part2()
 //    adventOfCodeYear2021Day06Part1()
 //    adventOfCodeYear2021Day06Part2()
-    adventOfCodeYear2021Day07Part1()
-//    adventOfCodeYear2021Day07Part2()
+//    adventOfCodeYear2021Day07Part1()
+    adventOfCodeYear2021Day07Part2()
 }
