8b2566f2547b083ef9c4e8068467d693656ecf25
Michiels <jarno.michiels@cegeka.com>
2023-12-04 08:35:20 +0100

    2023: Day 04

---
+++ src/main/kotlin/com/jarnocegeka/year2023/day04.kt
@@ -0,0 +1,69 @@
+package com.jarnocegeka.year2023
+
+import com.jarnocegeka.utils.readInputFileLines
+
+fun adventOfCodeYear2023Day04Part1() {
+    val lines = readInputFileLines("InputYear2023Day04.txt")
+    val scratchCards = mapToScratchCards(lines)
+    val result = validateScratchCards(scratchCards)
+
+    println("Result: $result")
+}
+
+fun adventOfCodeYear2023Day04Part2() {
+    val lines = readInputFileLines("InputYear2023Day04.txt")
+    val scratchCards = mapToScratchCards(lines)
+    val scratchCardsByCardId = scratchCards.associate { Pair(it.cardId, mutableListOf(it)) }
+    val result = calculateTotalAmountOfScratchCards(scratchCardsByCardId)
+
+    println("Result: $result")
+}
+
+private fun mapToScratchCards(lines: List<String>): List<ScratchCard> = lines.map { mapToScratchCard(it) }
+private fun mapToScratchCard(line: String): ScratchCard {
+    val cardId = line.substringAfter("Card").substringBefore(":").trim().toInt()
+
+    val winningNumbers = getWinningNumbers(line)
+    val chosenNumbers = getChosenNumbers(line)
+
+    return ScratchCard(cardId, winningNumbers, chosenNumbers)
+}
+
+private fun getWinningNumbers(line: String): List<Int> {
+    return line.substringAfter(":").substringBefore("|").trim().split(Regex("\\s+")).map { it.toInt() }
+}
+
+private fun getChosenNumbers(line: String): List<Int> {
+    return line.substringAfter("|").trim().split(Regex("\\s+")).map { it.toInt() }
+}
+
+private fun validateScratchCards(scratchCards: List<ScratchCard>): Int {
+    return scratchCards.map { it.calculateCardPoints() }.reduce { accumulator, points -> accumulator + points }
+}
+
+private fun calculateTotalAmountOfScratchCards(scratchCardsByCardId: Map<Int, MutableList<ScratchCard>>): Int {
+    scratchCardsByCardId.forEach { (cardId, scratchCards) ->
+        scratchCards.forEach { scratchCard ->
+            val winningChosenNumbersCount = scratchCard.winningChosenNumbersCount()
+            for (copiedCardId in cardId + 1..cardId + winningChosenNumbersCount) {
+                val scratchCardToCopy = scratchCardsByCardId[copiedCardId]!!.first()
+                scratchCardsByCardId[copiedCardId]!!.add(scratchCardToCopy.copy())
+            }
+        }
+    }
+
+    return scratchCardsByCardId.values.map { it.size }.reduce { accumulator, points -> accumulator + points }
+}
+
+private class ScratchCard(val cardId: Int, val winningNumbers: List<Int>, val chosenNumbers: List<Int>) {
+    fun winningChosenNumbers(): List<Int> = chosenNumbers.filter { winningNumbers.contains(it) }
+    fun winningChosenNumbersCount(): Int = chosenNumbers.count { winningNumbers.contains(it) }
+    fun copy(): ScratchCard = ScratchCard(cardId, winningNumbers, chosenNumbers)
+    fun calculateCardPoints(): Int {
+        val winningChosenNumbers = winningChosenNumbers()
+        if (winningChosenNumbers.isEmpty()) return 0
+
+        return winningChosenNumbers.drop(1).fold(1) { sum, _ -> sum * 2 }
+    }
+}
+
--- src/main/kotlin/com/jarnocegeka/year2023/main2023.kt
+++ src/main/kotlin/com/jarnocegeka/year2023/main2023.kt
@@ -5,6 +5,8 @@ fun main() {
 //    adventOfCodeYear2023Day01Part2()
 //    adventOfCodeYear2023Day02Part1()
 //    adventOfCodeYear2023Day02Part2()
-    adventOfCodeYear2023Day03Part1()
-    adventOfCodeYear2023Day03Part2()
+//    adventOfCodeYear2023Day03Part1()
+//    adventOfCodeYear2023Day03Part2()
+    adventOfCodeYear2023Day04Part1()
+    adventOfCodeYear2023Day04Part2()
 }
