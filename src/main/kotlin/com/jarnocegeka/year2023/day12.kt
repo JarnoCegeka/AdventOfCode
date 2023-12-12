6056e4804cc6d704f45fbc110443ff80b6ee4799
Michiels <jarno.michiels@cegeka.com>
2023-12-12 15:10:17 +0100

    2023: Day 12

--- src/main/kotlin/com/jarnocegeka/year2023/day12.kt
+++ src/main/kotlin/com/jarnocegeka/year2023/day12.kt
@@ -3,7 +3,6 @@ package com.jarnocegeka.year2023
 import com.jarnocegeka.utils.readInputFileLines
 import com.jarnocegeka.utils.reduceToLongWithSum
 import com.jarnocegeka.utils.whiteSpaceRegex
-import java.lang.IllegalStateException
 
 fun adventOfCodeYear2023Day12Part1() {
     val lines = readInputFileLines("year2023/InputYear2023Day12.txt")
@@ -16,8 +15,7 @@ fun adventOfCodeYear2023Day12Part2() {
     val lines = readInputFileLines("year2023/InputYear2023Day12.txt")
     val conditionRecords = mapToConditionRecords(lines)
     conditionRecords.forEach { it.unfoldConditionRecord() }
-    val arrangementCounts = conditionRecords.map { it.calculateArrangements() }
-    println(arrangementCounts.reduceToLongWithSum())
+    println(conditionRecords.map { it.calculateArrangements() }.reduceToLongWithSum())
 }
 
 private fun mapToConditionRecords(lines: List<String>): List<ConditionRecord> {
@@ -30,35 +28,30 @@ private fun mapToConditionRecord(line: String): ConditionRecord {
     return ConditionRecord(conditionRecordInfo[0], conditionRecordInfo[1].split(",").map { it.trim().toInt() })
 }
 
-private class BlockMapKey(vararg keys: Int)
 private class ConditionRecord(var springs: String, var contiguousGroup: List<Int>) {
+
     fun unfoldConditionRecord() {
         springs = (0 until 5).joinToString("?") { springs }
         contiguousGroup = (0 until 5).flatMap { contiguousGroup }
     }
 
-    fun calculateArrangements(i: Int = 0, j: Int = 0, cur: Int = 0): Long {
-        return calculateArrangements(mutableMapOf(), springs, contiguousGroup, i, j, cur)
+    fun calculateArrangements(): Long {
+        val cache = hashMapOf<Pair<String, List<Int>>, Long>()
+
+        return calculateArrangements(springs, contiguousGroup, cache)
     }
 
-    private fun calculateArrangements(blockMap: MutableMap<BlockMapKey, Long>, springs: String, contiguousGroup: List<Int>, i: Int, j: Int, cur: Int): Long {
-        val key = BlockMapKey(i, j, cur)
-        if (blockMap.contains(key)) return blockMap[key] ?: throw IllegalStateException("Couldn't find value for block key!")
+    private fun calculateArrangements(springs: String, contiguousGroup: List<Int>, cache: MutableMap<Pair<String, List<Int>>, Long>): Long {
+        if (contiguousGroup.isEmpty()) return if ("#" in springs) 0 else 1
+        if (springs.isEmpty()) return 0
 
-        if (i == springs.length) return if ((j == contiguousGroup.size && cur == 0) || (j == contiguousGroup.size - 1 && contiguousGroup[j] == cur)) 1 else 0
-
-        var total = 0L
-        val c = springs[i]
-        if ((c == '.' || c == '?') && cur == 0) {
-            total += calculateArrangements(blockMap, springs, contiguousGroup, i + 1, j, 0)
-        } else if ((c == '.' || c == '?') && cur > 0 && j < contiguousGroup.size && contiguousGroup[j] == cur) {
-            total += calculateArrangements(blockMap, springs, contiguousGroup, i + 1, j + 1, 0)
+        return cache.getOrPut(springs to contiguousGroup) {
+            var result = 0L
+            if (springs.first() in ".?")
+                result += calculateArrangements(springs.drop(1), contiguousGroup, cache)
+            if (springs.first() in "#?" && contiguousGroup.first() <= springs.length && "." !in springs.take(contiguousGroup.first()) && (contiguousGroup.first() == springs.length || springs[contiguousGroup.first()] != '#'))
+                result += calculateArrangements(springs.drop(contiguousGroup.first() + 1), contiguousGroup.drop(1), cache)
+            result
         }
-        if (c == '#' || c == '?') {
-            total += calculateArrangements(blockMap, springs, contiguousGroup, i + 1, j, cur + 1)
-        }
-
-        blockMap[key] = total
-        return total
     }
 }
